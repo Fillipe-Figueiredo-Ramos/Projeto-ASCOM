9429d8006db23c9136d5a9857675af50ebb66997
Gabriel Henrique Rabelo da Silva de Gusmão Lobo <gabriel.lobo@NOVACAP.SEDE>
2023-03-29 16:20:09 -0300

    atualizações

--- archive-avisos.php
+++ archive-avisos.php
@@ -31,8 +31,15 @@ require_once 'header.php';
     ?>
 
 </div>
-<img id="wave" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/wave-ai.svg' ?>" alt="">
-
+<div class="wave">
+    <div class="custom-shape-divider-bottom-1679004214">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                class="shape-fill"></path>
+        </svg>
+    </div>
+</div>
 <!--body-->
 
 <!--libras-->
@@ -47,22 +54,23 @@ require_once 'header.php';
     <?php $args = [
         'post_type' => 'aviso',
         'post_status' => 'publish',
-        'posts_per_page' => 'number of posts'
+        'posts_per_page' => '100000000000000000000000000'
     ];
 
     $my_posts = get_posts($args);
     foreach ($my_posts as $post) { ?>
-        <div class="CardAviso">
+        <div class="item-Aviso">
             <!--Imagem aviso-->
             <ul>
-                <li>
-                    <div class="Thumbnail-Aviso">
-                        <?php the_post_thumbnail(); ?>
+<!-- descomentar caso queiramos imagem com o aviso -->
+                <!-- <li>
+                    <div class="imagem-Item-Aviso">
+                        <?php //the_post_thumbnail(); ?>
                     </div>
-                </li>
+                </li> -->
                 <!--Título do aviso-->
                 <li>
-                    <p class="TittleAviso">
+                    <p class="titulo-Item-Aviso">
                         <?php echo $post->post_title; ?>
                     </p>
                 </li>
@@ -70,8 +78,7 @@ require_once 'header.php';
             </ul>
         </div>
     <?php } ?>
-    <a href="#" class="vejaTodos">Veja todos >>></a>
-</section>
+
 
 
 <?php
--- archive-noticias.php
+++ archive-noticias.php
@@ -3,7 +3,6 @@
 <?php
 require_once 'header.php';
 ?>
-
 <div class="view2 intro hm-purple-light" id="starter" style="z-index: 2;">
 
     <?php
@@ -31,7 +30,15 @@ require_once 'header.php';
     ?>
 
 </div>
-<img id="wave" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/wave-ai.svg' ?>" alt="">
+<div class="wave">
+    <div class="custom-shape-divider-bottom-1679004214">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                class="shape-fill"></path>
+        </svg>
+    </div>
+</div>
 
 <!--body-->
 
@@ -47,28 +54,28 @@ require_once 'header.php';
 <?php $args = [
         'post_type' => 'noticias',
         'post_status' => 'publish',
-        'posts_per_page' => 'number of posts'
+        'posts_per_page' => '100000000000000000000000000'
     ];
 
     $my_posts = get_posts($args);
     foreach ($my_posts as $post) { ?>
-        <div>
+        <div class="item-Noticia">
             <!--Imagem noticia-->
             <ul>
                 <li>
-                    <div>
+                    <div class="imagem-Item-Noticia">
                         <?php the_post_thumbnail(); ?>
                     </div>
                 </li>
                 <!--Título da noticia-->
                 <li>
-                    <p>
+                    <p class="titulo-Item-Noticia">
                         <?php echo $post->post_title; ?>
                     </p>
                 </li>
                 <!--Resumo da noticia-->
                 <li>
-                    <p>
+                    <p class="resumo-Item-Noticia">
                         <?php echo $post->post_excerpt; ?>
                     </p>
                 </li>
@@ -76,7 +83,7 @@ require_once 'header.php';
             </ul>
         </div>
     <?php } ?>
-</section>
+
 
 
 
