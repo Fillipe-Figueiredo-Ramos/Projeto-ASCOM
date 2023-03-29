56ebb95ad8e35bd260913e695410949f22787700
Gabriel Henrique Rabelo da Silva de Gusmão Lobo <gabriel.lobo@NOVACAP.SEDE>
2023-03-29 15:29:49 -0300

    inicio da pagina dos custom post types

---
+++ archive-avisos.php
@@ -0,0 +1,50 @@
+<?php /*Template Name: arquivo avisos */?>
+
+<?php
+require_once 'header.php';
+?>
+
+<div class="view2 intro hm-purple-light" id="starter" style="z-index: 2;">
+
+    <?php
+    the_custom_logo();
+
+    $args = array(
+        'post_type' => 'banners',
+        'post_status' => 'publish',
+        'posts_per_page' => 1,
+        'category_name' => 'administrativo'
+    );
+
+    $query = new WP_Query($args);
+    if ($query->have_posts()):
+        while ($query->have_posts()):
+            $query->the_post();
+    ?>
+    <div class="imagem-banner">
+        <?php the_post_thumbnail(); ?>
+    </div>
+    <?php
+        endwhile;
+    endif;
+
+    ?>
+
+</div>
+<img id="wave" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/wave-ai.svg' ?>" alt="">
+
+<!--body-->
+
+<!--libras-->
+<div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+        <div class="vw-plugin-top-wrapper"></div>
+    </div>
+</div>
+
+
+
+<?php
+require_once 'footer.php';
+?>
