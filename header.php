b6d13c6dfe952702099106b72ef3f35618efc05a
Fillipe Figueiredo Ramos <fillipe.ramos29@gmail.com>
2023-04-03 17:52:48 -0300

    Reestruturação do HTML

--- header.php
+++ header.php
@@ -29,10 +29,9 @@
 </head>
 
 <body>
-
     <!--nav azul-->
-    <header class="nav-azul">
-        <div class="Tudo-de-cima">
+    <header>
+        <div class="nav-azul">
             <div class="s130">
                 <form action="<?php echo esc_url(home_url()); ?>" method="get">
                     <div class="inner-form">
@@ -63,4 +62,54 @@
                 <a target="_blank" href="https://www.facebook.com/novacapoficial/" class="fa fa-facebook"></a>
                 <a target="_blank" href="https://twitter.com/novacap" class="fa fa-twitter"></a>
             </section>
+        </div>
+        <div>
+            <nav id="Menu-ps">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'menu' => 'Menu-principal',
+                        'menu_id' => 'Menu',
+                        'container_id' => 'nav-menu'
+
+                    )
+                );
+                ?>
+            </nav>
+            <div>
+                <?php
+                the_custom_logo();
+
+                $args = array(
+                    'post_type' => 'banners',
+                    'post_status' => 'publish',
+                    'posts_per_page' => 1,
+                    'category_name' => 'principal'
+                );
+
+                $query = new WP_Query($args);
+                if ($query->have_posts()):
+                    while ($query->have_posts()):
+                        $query->the_post();
+                        ?>
+                        <div class="imagem-banner">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                        <?php
+                    endwhile;
+                endif;
+                ?>
+            </div>
+            <div>
+                <div class="wave">
+                    <div class="custom-shape-divider-bottom-1679004214">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                            preserveAspectRatio="none">
+                            <path
+                                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                                class="shape-fill"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
     </header>
